de800b92c861c877c7e37918e00230b0e3cf0054
larry <xlarry1000@gmail.com>
2020-06-25 15:49:31 +0800

    About page

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,10 @@
 <template>
-    <div>
-        123
-    </div>
+  <div>
+    <h1>關於我</h1>
+    <!-- <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRLJf9M3fZQ82WKEHDwlMEMvBEkSpiw2lc9lQ&usqp=CAU" alt="" /> -->
+    <p>
+      你好我是拉瑞
+    </p>
+    <a href="https://github.com/larrykkk" target="_blank">Github</a>
+  </div>
 </template>
